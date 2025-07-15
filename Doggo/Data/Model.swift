d0404c74ca726b2fc10bc2cfd37f63ac684f499e
Trey G <dove-ascent-0q@icloud.com>
2025-07-15 17:50:11 -0400

    Structure

---
+++ Doggo/Data/Model.swift
@@ -0,0 +1,7 @@
+//
+//  Model.swift
+//  Doggo
+//
+//  Created by Trey Gaines on 7/15/25.
+//
+
---
+++ Doggo/Data/Observable.swift
@@ -0,0 +1,7 @@
+//
+//  Observable.swift
+//  Doggo
+//
+//  Created by Trey Gaines on 7/15/25.
+//
+
