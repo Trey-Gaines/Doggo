997b59ce2c3a0a37000af96032ae731099d6ce40
Trey G <dove-ascent-0q@icloud.com>
2025-07-15 12:47:36 -0400

    Initial Commit

---
+++ Doggo/DoggoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoggoApp.swift
+//  Doggo
+//
+//  Created by Trey Gaines on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct DoggoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
